745c31faec25bc97ad78b2aca662715ed5f74fe0
Ramakrishna Narayanan <naramakrishna@hotmail.com>
2022-09-17 17:15:53 +0530

    avoid duplication of post

--- part 2/app.py
+++ part 2/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -9,15 +9,17 @@ items = []
 
 class Item(Resource):
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return{'Item': item}
-        return {'Item':None}, 404
+        item = next(filter(lambda x: x['name'] == name, items),None)
+            
+        return {'Item':item}, 200 if item is not None else 400
     
     def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None) is not None:
+            return {'message' : "A item with the name '{}' already exists".format(name)}, 400
+        data = request.get_json()
         item = {
             'name': name,
-            'price': 12.00
+            'price': data['price']
         }
         items.append(item)
         return item, 201
