d9f33a9a7d8933cd97fb9b35e7e4884f34a861a5
Ramakrishna Narayanan <naramakrishna@hotmail.com>
2022-09-17 18:13:15 +0530

    parser addition

--- part 2/app.py
+++ part 2/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
 
@@ -8,6 +8,14 @@ api = Api(app)
 items = []
 
 class Item(Resource):
+    
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+                        type=float,
+                        required=True,
+                        help="Thi s field cannot be blank"
+                        )
+        
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items),None)
             
@@ -16,13 +24,35 @@ class Item(Resource):
     def post(self, name):
         if next(filter(lambda x: x['name'] == name, items), None) is not None:
             return {'message' : "A item with the name '{}' already exists".format(name)}, 400
-        data = request.get_json()
+        
+        data = Item.parser.parse_args()
         item = {
             'name': name,
             'price': data['price']
         }
         items.append(item)
         return item, 201
+    
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return  {'message': "A item with the name '{}' deleted".format(name)}, 200
+    
+    def put(self, name):
+        
+        data = Item.parser.parse_args()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {
+                'name': name,
+                'price': data['price']
+            }
+            items.append(item)
+            return item, 201
+        else:
+            
+            item.update(data)
+        return item
 
 class ItemList(Resource):
     def get(self):
